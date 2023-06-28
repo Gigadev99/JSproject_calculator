fb054c55adfb5e3afb638e132fe5e2f37cc2661c
Gigadev99 <124036859+Gigadev99@users.noreply.github.com>
2023-06-28 16:59:03 +0800

    added ability to calutate 2 numbers only

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
-function add(a,b) {
-    return a + b
-}
-function subtract(a,b) {
-    return a - b
-}
-function multiply(a,b) {
-    return a * b 
-}
-function divide(a,b) {
-    return a / b
-}
 function operate(a, sign, b) {
-    if (sign == "+") {
-        return add(a,b)
+    if (sign == "add") {
+        return a + b
     }
-    if (sign == "-") {
-        return subtract(a,b)
+    if (sign == "subtract") {
+        return a - b
     }
-    if (sign == "*") {
-        return multiply(a,b)
+    if (sign == "multiply") {
+        return a * b
     }
-    if (sign == "/") {
-        return divide(a,b)
+    if (sign == "divide") {
+        return a / b
     }
 }
+var num1;
+var num2;
+var signflag;
+var operation;
 
 const screen = document.querySelector(".screen")
 function display(number) {
-    screen.textContent = screen.textContent.concat(number)
-    if (screen.textContent.includes('.') == true) {
-        document.getElementById('.').removeEventListener('click', anon)
+    if (signflag == 1) {
+        signs.forEach(sign => sign.removeEventListener('click', input))
+    }
+    screen.textContent += `${number}`
+    if (screen.textContent.includes('.')) {
+        document.getElementById('.')
+        .removeEventListener('click', input)
     }
 }
-const anon = function(e) {
-    display(e.target.id) 
+const input = function(e) {
+    if (e.target.className.includes('sign')) {  
+        num1 = Number(screen.textContent)
+        signflag = 1;
+        screen.textContent = ""    
+        display(e.target.textContent)
+        operation = e.target.id
+        return
+    }
+    if (signflag == 1 && e.target.className.includes('number')) {
+        signflag = 0;
+        screen.textContent = e.target.textContent
+        return
+    }
+    if (e.target.id == "equals") {
+        num2 = Number(screen.textContent)
+        screen.textContent = operate(num1, operation, num2)
+        return
+    }
+    display(e.target.textContent);
 }
 const numbers = document.querySelectorAll(".number")
-numbers.forEach(number => number.addEventListener('click', anon))
+numbers.forEach(number => number.addEventListener('click', input))
 
+const signs = document.querySelectorAll(".sign")
+signs.forEach(sign => sign.addEventListener('click', input))
+const equals = document.querySelector('#equals')
+equals.addEventListener('click', input)
