d40f11edc03e443a4c65ae0b83af2ce1624f494b
Gigadev99 <124036859+Gigadev99@users.noreply.github.com>
2023-07-11 00:59:02 +0800

    fixed bugs and nicer code

--- script.js
+++ script.js
@@ -1,51 +1,34 @@
 function operate(a, operation, b) {
-    if (operation == "add") {
-        return a + b
-    }
-    if (operation == "subtract") {
-        return a - b
-    }
-    if (operation == "multiply") {
-        return a * b
-    }
-    if (operation == "divide") {
-        return a / b
-    }
-    return a
-}
-var num1;
-var num2;
-var num3
-var signflag;
-var operation;
-
+    if (operation == "add") return a + b
+    if (operation == "subtract") return a - b
+    if (operation == "multiply") return a * b
+    if (operation == "divide") return a / b
+    return b // if operation is undefined, just return the same value 
+}            // as is on the screen when equals pressed
+var num1, num2, num3, signflag, operation, ans;
 const screen = document.querySelector(".screen")
 function display(number) {
-    if (signflag == 1) {
-        signs.forEach(sign => sign.removeEventListener('click', input))
-    }
     screen.textContent += `${number}`
     if (screen.textContent.includes('.')) {
-        document.getElementById('.')
-        .removeEventListener('click', input)
+        dot.removeEventListener('click', input) // so multiple dots cant be typed once
     }
 }
 const input = function(e) {
     if (e.target.className.includes('sign') && signflag == 0) {
         num2 = Number(screen.textContent)
-        screen.textContent = operate(num1, operation, num2)
+        ans = operate(num1, operation, num2)
+        screen.textContent = Number(ans.toFixed(9))
         num1 = screen.textContent
         signflag = 1;
         operation = e.target.id
+        dot.addEventListener('click', input)
     }
     else if (e.target.className.includes('sign')) {  
         num3 = Number(screen.textContent)
-        if (!isNaN(num3)) {
-            num1 = num3
-        } 
-        signflag = 1;
+        if (!isNaN(num3)) {num1 = num3}  // this ignores the screen if operation is pressed multiple times
+        signflag = 1;                    // so that num1 isn't assigned an operator!
         screen.textContent = e.target.textContent
-        operation = e.target.id
+        operation = e.target.id;            dot.addEventListener('click', input)
         return
     }
     if (screen.textContent == 0 && e.target.id != "equals" ) {
@@ -59,24 +42,15 @@ const input = function(e) {
     }
     if (e.target.id == "equals") {
         num2 = Number(screen.textContent)
-        if (operation == undefined) {
-            screen.textContent = num2; return
-        }
-        screen.textContent = operate(num1, operation, num2)
-        signflag = undefined; // bad practice but atleast it works
+        ans = operate(num1, operation, num2)
+        screen.textContent = Number(ans.toFixed(9))
+        signflag = undefined // bad practice but atleast it works
+        operation = undefined
         return
-    }
-    if (e.target.id == "equals" && (screen.textContent == 0 ||
-        num1 == undefined || num2 == undefined)) {
-    }
-    
+    } 
     display(e.target.textContent);
 }
-const numbers = document.querySelectorAll(".number")
-numbers.forEach(number => number.addEventListener('click', input))
-
+const buttons = document.querySelectorAll(".button")
+buttons.forEach(number => number.addEventListener('click', input))
 const signs = document.querySelectorAll(".sign")
-signs.forEach(sign => sign.addEventListener('click', input))
-
-const equals = document.querySelector('#equals')
-equals.addEventListener('click', input)
+const dot = document.getElementById(".")
