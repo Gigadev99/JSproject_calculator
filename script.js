7770a004ccd1e736390b7e09526ca818d50198a7
Gigadev99 <124036859+Gigadev99@users.noreply.github.com>
2023-07-18 13:45:54 +0800

    added keyboard support

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 function operate(a, operation, b) {
-    if (operation == "add") return a + b
-    if (operation == "subtract") return a - b
-    if (operation == "multiply") return a * b
-    if (operation == "divide") return a / b
+    if (operation == "+") return Number(a) + b //so a doesnt get concatenated when its a string
+    if (operation == "-") return a - b
+    if (operation == "*") return a * b
+    if (operation == "/") return a / b
     return b // if operation is undefined, just return the same value 
 }            // as is on the screen when equals pressed
 var num1, num2, num3, signflag, operation, ans;
 const screen = document.querySelector(".screen")
 function display(number) {
     screen.textContent += `${number}`
-    if (screen.textContent.includes('.')) {
-        dot.removeEventListener('click', input) // so multiple dots cant be typed once
-    }
 }
 const input = function(e) {
     if (e.target.className.includes('sign') && signflag == 0) {
         num2 = Number(screen.textContent)
         ans = operate(num1, operation, num2)
-        screen.textContent = Number(Number(ans).toFixed(9)) //sometimes tofixed doesnt work as ans is a string so convert it to number first
+        screen.textContent = Number(Number(ans).toFixed(9)) //sometimes toFixed doesnt work as ans is a string so convert it to number first
         num1 = screen.textContent // then convert the result to number again to remove unnecessary zeroes (.000)
         signflag = 1;
         operation = e.target.id
@@ -31,7 +28,9 @@ const input = function(e) {
         operation = e.target.id;            dot.addEventListener('click', input)
         return
     }
-    if (screen.textContent == 0 && e.target.id != "equals" ) {
+    if (e.target.id == ".") dot.removeEventListener('click', input) // so multiple dots can't be typed once
+    if (screen.textContent == 0 && e.target.id != "=" && e.target.id != "." 
+        && !screen.textContent.includes('.')) /* for the edge case when display is "0." */ {
         screen.textContent = e.target.textContent
         return
     }
@@ -40,17 +39,23 @@ const input = function(e) {
         screen.textContent = e.target.textContent
         return
     }
-    if (e.target.id == "equals") {
+    if (e.target.id == "=") {
         num2 = Number(screen.textContent)
         ans = operate(num1, operation, num2)
-        screen.textContent = Number(Number(ans).toFixed(9))
+        screen.textContent = Number(ans).toFixed(9)
         signflag = undefined // bad practice but atleast it works
         operation = undefined
         return
-    } 
-    display(e.target.textContent);
+    }
+    display(e.target.textContent); 
 }
 const buttons = document.querySelectorAll(".button")
-buttons.forEach(number => number.addEventListener('click', input))
-const signs = document.querySelectorAll(".sign")
+buttons.forEach(button => button.addEventListener('click', input))
 const dot = document.getElementById(".")
+//Keyboard section
+document.addEventListener('keydown', e => {
+    let button = document.getElementById(e.key);
+    if (button == undefined) return;
+    button.click();
+    console.log(e.key)
+})
